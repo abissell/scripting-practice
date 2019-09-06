7972460a1dd291cba142f40c74e835181659f65f
Andrew Bissell <bissell@adobe.com>
2019-09-06 14:58:12 -0700

    more work on backup utility shell script

--- sh/backup.sh
+++ sh/backup.sh
@@ -3,25 +3,46 @@
 backup_dir() {
     files=""
 
-    for file in $(find $1 -d 1)
+    source=$1
+    target=$2
+
+    echo "source: $source"
+    echo "target: $target"
+
+    for file in $(find $source -d 1)
     do
+        echo "Looking at file: $file"
+        echo "source: $source"
+        echo "target: $target"
         if [[ -f $file ]] ; then
             files="$files $file"
         elif [[ -d $file ]] ; then
-            backup_dir $file
+            relativeDir=${file:${#source}:${#file}}
+            echo "relativeDir: $relativeDir"
+            targetDir="$target$relativeDir"
+            if [[ -e $targetDir && ! -d $targetDir ]] ; then
+                echo "Target directory $targetDir already exists but is not a directory!"
+                exit 1
+            fi
+            echo "mkdir: $targetDir"
+            mkdir $targetDir
+            echo "Calling backup_dir with source: $file target: $targetDir"
+            backup_dir $file $targetDir
         fi
     done
 
     if [[ files != "" ]] ; then
-        echo "Files in directory: $files"
+        destFile="$target/BACKUP.tar.gz"
+        echo "tar -czvf $destFile $files"
+        # tar -czvf $destFile $files
     fi
+
+    echo "exiting backup_dir"
 }
 
-if [[ -e $2 && ! -d $2 ]] ; then
-    echo "Target $2 already exists but is not a directory!"
-    exit 1
-fi
+source=$1
+target=$2
 
-backup_dir "$@"
+backup_dir $source $target
 
 exit 0
